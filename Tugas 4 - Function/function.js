6c5a796ebf3bc9497c9ea3775dd3c2c8713b0d06
Robbin Nurhuda <nurhudarobbin@gmail.com>
2020-11-05 16:52:59 +0800

    Tugas Day 4

---
+++ Tugas 4 - Function/function.js
@@ -0,0 +1,28 @@
+
+// Soal No. 1
+function teriak() {
+    return "Halo Sanbers!"
+}
+
+console.log(teriak())
+
+// Soal No. 2
+function kalikan(num1, num2){
+    var num1 = 12
+    var num2 = 4
+    return num1 * num2
+}
+
+var hasilKali = kalikan()
+console.log(hasilKali);
+
+// Soal No. 3
+function introduce(name, age, address, hobby) {
+    var name = "Robbin"
+    var age = 22
+    var address = "Jln. Haji Mandor, Cilandak, Jakarta Selatan"
+    var hobby = "Gaming"
+    return "Nama saya " + name + ", Umur saya " + age + " tahun, Alamat saya di " + address + ", dan Hobby saya adalah " + hobby + "!";
+}
+var perkenalan = introduce()
+console.log(perkenalan);
